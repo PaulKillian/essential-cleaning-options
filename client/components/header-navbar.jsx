7c0ba8377fc95e9251ec6de99dfdeee0857b18a0
PaulKillian <psk65lava@gmail.com>
2020-10-27 12:40:16 -0700

    initial files and header/navbar

---
+++ client/components/header-navbar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import 'bootstrap';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: []
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <div className="shadow-sm col">
+          <div className="row d-flex justify-content-between">
+            <h3 className="col mt-2">ECO</h3>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white">
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul className="navbar-nav">
+                  <li className="nav-item active">
+                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                      data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">What I Do</a>
+                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <a className="dropdown-item" href="#">Carpet Cleaning</a>
+                      <a className="dropdown-item" href="#">Area Rug Cleaning</a>
+                      <a className="dropdown-item" href="#">Furniture/Upholstrey Cleaning</a>
+                      <a className="dropdown-item" href="#">Tile/Grout Cleaning</a>
+                      <a className="dropdown-item" href="#">Auto Detailing</a>
+                    </div>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Reviews</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Header;
