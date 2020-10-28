7c5c84531caf66c820561985cd123d91387a9368
PaulKillian <psk65lava@gmail.com>
2020-10-28 15:22:03 -0700

    finshed basic

--- client/components/main-content.jsx
+++ client/components/main-content.jsx
@@ -3,11 +3,35 @@ import React from 'react';
 function MainContent() {
   return (
     <>
-      <div id="background" className="bg-mute">
-        <div className="d-flex flex-wrap justify-content-around">
-          <div id="main-items" className="bg-white col-3 shadow-sm mt-5">Hello</div>
-          <div id="main-items" className="bg-white col-3 shadow-sm mt-5">Hello</div>
-          <div id="main-items" className="bg-white col-3 shadow-sm mt-5">Hello</div>
+      <div id="background">
+        <div className="bg-mute d-flex justify-content-around">
+          <div className="card w-25 mx-3 mt-5 shadow-sm">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">Some quick example text to build on the </p>
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+          </div>
+          <div className="card w-25 mx-3 mt-5 shadow-sm">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">Some quick example text to build on the </p>
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+          </div>
+          <div className="card w-25 mx-3 mt-5 shadow-sm">
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+              <p className="card-text">Some quick example text to build on the </p>
+              <a href="#" className="card-link">Card link</a>
+              <a href="#" className="card-link">Another link</a>
+            </div>
+          </div>
         </div>
       </div>
     </>
