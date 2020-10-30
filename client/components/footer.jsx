0aa9ca3815f584f6febcf1ec0920dd62f3003240
PaulKillian <psk65lava@gmail.com>
2020-10-30 08:14:14 -0700

    header, hero, cards

---
+++ client/components/footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="frame-5 border-class-1">
+        <div className="rectangle-24-C61RwL border-class-1"></div>
+        <div className="rectangle-24-VMr6Om"></div>
+        <div className="copyright--en-mendoza font-class-1 border-class-1">Copyright 2020 Ken Mendoza</div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
