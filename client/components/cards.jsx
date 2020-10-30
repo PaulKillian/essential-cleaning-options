4d1ec76014a7db0f74e60cee78a5f5db30d8dea2
PaulKillian <psk65lava@gmail.com>
2020-10-30 08:14:14 -0700

    header, hero, cards

---
+++ client/components/cards.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Cards() {
+  return (
+    <>
+      <div className="row d-flex justify-content-around mt-5">
+        <div className="card w-25">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the.</p>
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+        </div>
+        <div className="card w-25">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the.</p>
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+        </div>
+        <div className="card w-25">
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the.</p>
+            <a href="#" className="card-link">Card link</a>
+            <a href="#" className="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Cards;
