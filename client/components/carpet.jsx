3f4dbb2ed8fff310e412e627866e4858b3749b20
PaulKillian <psk65lava@gmail.com>
2020-11-03 16:01:26 -0800

    add estimate contact form

--- client/components/carpet.jsx
+++ client/components/carpet.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import AOS from 'aos';
+
 function Carpet() {
+  AOS.init();
   return (
     <>
       <div className="d-flex justify-content-center">
-        <p className="position-absolute text-font mt-5 text-white">CARPET CLEANING</p>
-        <img className="w-100 carpet-h position-relative" src="/images/b1.jpg"></img>
+        <p className="position-absolute text-font mt-5 pt-5 text-white">CARPET CLEANING</p>
+        <img className="w-100 carpet-h position-relative" src="/images/bcarpet.jpg"></img>
+      </div>
+      <div className="row d-flex mt-5 justify-content-center">
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/gDUOQQSSbl0"
+          frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope;
+        picture-in-picture" allowFullscreen>
+        </iframe>
       </div>
     </>
   );
