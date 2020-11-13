89a49f7a22dcfe4ac79bf85f1a382b4653a202a7
PaulKillian <psk65lava@gmail.com>
2020-11-12 17:15:11 -0800

    added more depth

--- client/components/cards.jsx
+++ client/components/cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLeaf, faFighterJet, faTintSlash } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faLeaf, faFighterJet, faTintSlash } from '@fortawesome/free-solid-svg-icons';
 import AOS from 'aos';
 
 AOS.init();
@@ -12,45 +12,49 @@ function Cards() {
         <div data-aos="fade-up" data-aos-easing="linear"
           data-aos-duration="500" className="d-flex justify-content-center mt-5">
           <div className="col-12 bar-color shadow-sm">
-            <h3 className="text-center mt-2">VERY LOW MOISTURE...DRIES FAST...ECO FRIENDLY</h3>
+            <h3 className="text-center text-white mt-2">VERY LOW MOISTURE...DRIES FAST...ECO FRIENDLY</h3>
           </div>
         </div>
-        <div data-aos="flip-up" data-aos-easing="linear"
-          data-aos-duration="500" className="row d-flex justify-content-center mt-5 box text-dark">
-          <div data-aos="flip-up" data-aos-easing="linear"
-            data-aos-duration="500" className="card w-25 mr-3 shadow-sm border border-white">
-            <div className="card-body">
-              <div className="d-flex">
-                <h5 id="moisture" className="card-title mr-3">Very Low Moisture</h5>
-                <FontAwesomeIcon icon={faTintSlash} size="2x" className="font-color" />
+        <div>
+          <div className="bg-off-white mb-5 d-flex">
+            <div data-aos="flip-up" data-aos-easing="linear"
+              data-aos-duration="500" className="row d-flex justify-content-center mt-5 box text-dark">
+              <div className="d-flex justify-content-center mt-2">
+                <div className="card">
+                  <img className="card-img-top w-100 shadowed" src="images/leafw.png" alt="Card image cap"></img>
+                  <div className="card-body">
+                    <h4 className="card-title"><strong>REMOTE SUPPORT</strong></h4>
+                    <h5 className="blue-text pb-2"><strong>From Anywhere</strong></h5>
+                    <p className="card-text">Need assistance to a problem that wont go away. Try our Remote support option to save time and money</p>
+                  </div>
+                </div>
               </div>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the.</p>
-              <a href="#" className="card-link">Card link</a>
-              <a href="#" className="card-link">Another link</a>
             </div>
-          </div>
-          <div data-aos="flip-up" data-aos-easing="linear"
-            data-aos-duration="500" className="card w-25 mr-3 shadow-sm border border-white">
-            <div className="card-body">
-              <div className="d-flex">
-                <h5 className="card-title mr-3">Dries Fast</h5>
-                <FontAwesomeIcon icon={faFighterJet} size="2x" className="font-color" />
+            <div data-aos="flip-up" data-aos-easing="linear"
+              data-aos-duration="500" className="row d-flex justify-content-center mt-5 box text-dark">
+              <div className="d-flex justify-content-center mt-2">
+                <div className="card">
+                  <img className="card-img-top w-100 shadowed" src="images/fast.png" alt="Card image cap"></img>
+                  <div className="card-body">
+                    <h4 className="card-title"><strong>REMOTE SUPPORT</strong></h4>
+                    <h5 className="blue-text pb-2"><strong>From Anywhere</strong></h5>
+                    <p className="card-text">Need assistance to a problem that wont go away. Try our Remote support option to save time and money</p>
+                  </div>
+                </div>
               </div>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the.</p>
-              <a href="#" className="card-link">Card link</a>
-              <a href="#" className="card-link">Another link</a>
             </div>
-          </div>
-          <div data-aos="flip-up" data-aos-easing="linear"
-            data-aos-duration="500" className="card w-25 shadow-sm border border-white">
-            <div className="card-body">
-              <div className="d-flex">
-                <h5 className="card-title mr-3">Eco Friendly</h5>
-                <FontAwesomeIcon icon={faLeaf} size="2x" className="font-color" transform={{ rotate: 42 }} />
+            <div data-aos="flip-up" data-aos-easing="linear"
+              data-aos-duration="500" className="row d-flex justify-content-center mt-5 box text-dark">
+              <div className="d-flex justify-content-center mt-2">
+                <div className="card">
+                  <img className="card-img-top w-100 shadowed" src="images/leafw.png" alt="Card image cap"></img>
+                  <div className="card-body">
+                    <h4 className="card-title"><strong>REMOTE SUPPORT</strong></h4>
+                    <h5 className="blue-text pb-2"><strong>From Anywhere</strong></h5>
+                    <p className="card-text">Need assistance to a problem that wont go away. Try our Remote support option to save time and money</p>
+                  </div>
+                </div>
               </div>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the.</p>
-              <a href="#" className="card-link">Card link</a>
-              <a href="#" className="card-link">Another link</a>
             </div>
           </div>
         </div>
--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -7,26 +7,26 @@ class Footer extends React.Component {
   render() {
     return (
       <>
-        <img data-aos="fade-down"
-          data-aos-offset="300"
-          data-aos-easing="ease-in-sine" className="position-absolute footer-color" src="/images/foot.png"></img>
-        <div data-aos="fade-down"
-          data-aos-offset="300"
-          data-aos-easing="ease-in-sine" className="col-12 pl-5 text-white position-relative mb-5">
-          <div className="pt-4">
-            <div>
-              <img className="mb-2 ml-4 icon-size" src="/images/telephone.png"></img>
-              <div>555-234-2938</div>
+        <div className="d-flex justify-content-center">
+          <img className="shadowed-text position-absolute info-dimensions pb-5" src="/images/business.png"></img>
+          <div data-aos="fade-down"
+            data-aos-offset="300"
+            data-aos-easing="ease-in-sine" className="col-12 pl-5 text-white position-relative mb-5">
+            <div className="pt-4 d-flex">
+              <div>
+                <img className="mb-2 ml-4 icon-size" src="/images/telephone.png"></img>
+                <div>555-234-2938</div>
+              </div>
+              <div className="left-footer-image mt-5 mb-5">
+                <img src="/images/ken.jpg"></img>
+              </div>
+              <div>
+                <img className="ml-4 icon-size" src="/images/mail.png"></img>
+                <div>essentialcleaningoptions@gmail.com</div>
+              </div>
             </div>
-            <div className="left-footer-image mt-5 mb-5">
-              <img src="/images/ken.jpg"></img>
-            </div>
-            <div>
-              <img className="ml-4 icon-size" src="/images/mail.png"></img>
-              <div>essentialcleaningoptions@gmail.com</div>
-            </div>
+            <div className="text-center pb-2">Copyright 2020 Ken Mendoza</div>
           </div>
-          <div className="text-center pb-2">Copyright 2020 Ken Mendoza</div>
         </div>
       </>
     );
