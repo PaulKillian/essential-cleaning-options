495fd023ca621f8d03fafb9172e502aed28ba047
PaulKillian <psk65lava@gmail.com>
2020-11-20 14:14:24 -0800

    adjustments

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -7,27 +7,21 @@ class Footer extends React.Component {
   render() {
     return (
       <>
-        <img data-aos="fade-down"
-          data-aos-offset="300"
-          data-aos-easing="ease-in-sine" className="position-absolute footer-color" src="/images/foot.png"></img>
-        <div data-aos="fade-down"
-          data-aos-offset="300"
-          data-aos-easing="ease-in-sine" className="col-12 pl-5 text-white position-relative mb-5">
-          <div className="pt-4">
+        <footer className="d-flex justify-content-around text-white pt-3 mt-5">
+          <div className="">
             <div>
               <img className="mb-2 ml-4 icon-size" src="/images/telephone.png"></img>
               <div>555-234-2938</div>
             </div>
-            <div className="left-footer-image mt-5 mb-5">
-              <img src="/images/ken.jpg"></img>
-            </div>
-            <div>
-              <img className="ml-4 icon-size" src="/images/mail.png"></img>
-              <div>essentialcleaningoptions@gmail.com</div>
-            </div>
           </div>
-          <div className="text-center pb-2">Copyright 2020 Ken Mendoza</div>
-        </div>
+          <div className="mt-5 mb-5">
+            <img src="/images/ken.jpg"></img>
+          </div>
+          <div className="">
+            <img className="ml-4 icon-size" src="/images/mail.png"></img>
+            <div>essentialcleaningoptions@gmail.com</div>
+          </div>
+        </footer>
       </>
     );
   }
