afcf5a3681cc51673dfbb0bb225579931f25c201
PaulKillian <psk65lava@gmail.com>
2020-11-24 16:40:32 -0800

    further updates

--- client/components/auto.jsx
+++ client/components/auto.jsx
@@ -7,9 +7,21 @@ function Auto() {
     <>
       <div className="col-12 mb-5 scale bottom">
         <div className="d-flex justify-content-center align-items-center flex-column">
-          <p className=" text-font mt-4 shadowed-text text-dark">AUTO DETAILING</p>
+          <p className="text-font mt-4 shadowed-text text-white">AUTO DETAILING</p>
           <img className="w-50 h-75 mt-3 shadowed" src="/images/auto-detailing.png"></img>
         </div>
+      </div>
+      <div className="flex row mx-3 my-4 shadowed justify-content-around">
+        <img className="mt-4 col-3" src="images/rich1.jpg"></img>
+        <img className="mt-4 col-3" src="images/rich2.jpg"></img>
+        <img className="mt-4 col-3" src="images/rich3.jpg"></img>
+        <img className="mt-4 col-3" src="images/rich4.jpg"></img>
+      </div>
+      <div className="flex row mx-3 shadowed my-4 justify-content-around">
+        <img className="mt-4 col-3" src="images/guy1.jpg"></img>
+        <img className="mt-4 col-3" src="images/guy2.jpg"></img>
+        <img className="mt-4 col-3" src="images/guy3.jpg"></img>
+        <img className="mt-4 col-3" src="images/guy4.jpg"></img>
       </div>
     </>
   );
