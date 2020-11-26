aed59cf8aa7f261d238e149065acd90d3f5a7405
PaulKillian <psk65lava@gmail.com>
2020-11-25 16:38:22 -0800

    further updates

--- client/components/cards.jsx
+++ client/components/cards.jsx
@@ -9,24 +9,24 @@ function Cards() {
   return (
     <>
       <div className="d-flex justify-content-center display-4 mt-5">
-        <div data-aos="fade-up" data-aos-duration="2000" className="mr-5 text-white shadowed col-5 carpet-s containTL center-card-text">Carpet
+        <div data-aos="fade-up" data-aos-duration="2000" className="mr-5 text-white simple shadowed col-5 carpet-s containTL center-card-text">Carpet
           <div className="overlayL">
             <div className="text shadowed">Carpet</div>
           </div>
         </div>
-        <div data-aos="fade-up" data-aos-duration="2000" className="text-white shadowed col-5 bg-primary upholstrey-s containTR center-card-text">Furniture/Upholstrey
+        <div data-aos="fade-up" data-aos-duration="2000" className="text-white simple shadowed col-5 upholstrey-s containTR center-card-text">Furniture/Upholstrey
           <div className="overlayR">
             <div className="text shadowed-text">Furniture/Upholstrey</div>
           </div>
         </div>
       </div>
       <div className="d-flex justify-content-center display-4 mt-5">
-        <div data-aos="fade-bottom" data-aos-duration="2000" className="mr-5 text-white shadowed col-5 area-s containBL center-card-text">Area Rug
+        <div data-aos="fade-bottom" data-aos-duration="2000" className="mr-5 text-white simple shadowed col-5 area-s containBL center-card-text">Area Rug
           <div className="overlayL">
             <div className="text">Area Rug</div>
           </div>
         </div>
-        <div data-aos="fade-bottom" data-aos-duration="2000" className="text-white col-5 shadowed grout-s containBR center-card-text">Tile/Grout
+        <div data-aos="fade-bottom" data-aos-duration="2000" className="text-white simple col-5 shadowed grout-s containBR center-card-text">Tile/Grout
           <div className="overlayR">
             <div className="text">Tile/Grout</div>
           </div>
