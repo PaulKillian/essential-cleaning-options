247feb3ec618abb2c120080b50df11e8dea3d768
PaulKillian <psk65lava@gmail.com>
2020-11-30 16:52:33 -0800

    optimization

--- client/components/cards.jsx
+++ client/components/cards.jsx
@@ -1,4 +1,27 @@
 import React from 'react';
+import Gallery from 'react-grid-gallery';
+
+function carpetGallery() {
+  const IMAGES =
+    [{
+      src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
+      thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
+      thumbnailWidth: 320,
+      thumbnailHeight: 174,
+      isSelected: true
+    },
+    {
+      src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
+      thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
+      thumbnailWidth: 320,
+      thumbnailHeight: 174,
+      isSelected: true
+    }];
+
+  return (
+    <Gallery images={IMAGES} />
+  );
+}
 
 function Cards() {
   return (
@@ -6,7 +29,7 @@ function Cards() {
       <div className="d-flex justify-content-center display-4 mt-5">
         <div data-aos="fade-up" data-aos-duration="2000" className="mr-5 text-white simple shadowed col-5 carpet-s containTL center-card-text">Carpet
           <div className="overlayL">
-            <div className="text shadowed">Carpet</div>
+            <div onClick={carpetGallery()} className="text shadowed">Carpet</div>
           </div>
         </div>
         <div data-aos="fade-up" data-aos-duration="2000" className="text-white simple shadowed col-5 upholstrey-s containTR center-card-text">Furniture/Upholstrey
