ec2a03753f55ae21ab7612c238d4867b59856d53
PaulKillian <psk65lava@gmail.com>
2020-11-30 16:25:36 -0800

    optimization

--- client/components/cards.jsx
+++ client/components/cards.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faLeaf, faFighterJet, faTintSlash } from '@fortawesome/free-solid-svg-icons';
-import AOS from 'aos';
-
-AOS.init();
 
 function Cards() {
   return (
