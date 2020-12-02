2da313689b9f6583aa693e8f15edc15767fc8825
PaulKillian <psk65lava@gmail.com>
2020-12-01 17:37:36 -0800

    galleries added

--- client/components/cards.jsx
+++ client/components/cards.jsx
@@ -1,27 +1,5 @@
 import React from 'react';
-import Gallery from 'react-grid-gallery';
-
-function carpetGallery() {
-  const IMAGES =
-    [{
-      src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
-      thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
-      thumbnailWidth: 320,
-      thumbnailHeight: 174,
-      isSelected: true
-    },
-    {
-      src: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg',
-      thumbnail: 'https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_n.jpg',
-      thumbnailWidth: 320,
-      thumbnailHeight: 174,
-      isSelected: true
-    }];
-
-  return (
-    <Gallery images={IMAGES} />
-  );
-}
+import { NavLink } from 'react-router-dom';
 
 function Cards() {
   return (
@@ -29,12 +7,14 @@ function Cards() {
       <div className="d-flex justify-content-center display-4 mt-5">
         <div data-aos="fade-up" data-aos-duration="2000" className="mr-5 text-white simple shadowed col-5 carpet-s containTL center-card-text">Carpet
           <div className="overlayL">
-            <div onClick={carpetGallery()} className="text shadowed">Carpet</div>
+            <div className="text shadowed">Carpet</div>
+            <NavLink to="/carpet-gallery" className="nav-link text-white text-left">Photos</NavLink>
           </div>
         </div>
         <div data-aos="fade-up" data-aos-duration="2000" className="text-white simple shadowed col-5 upholstrey-s containTR center-card-text">Furniture/Upholstrey
           <div className="overlayR">
             <div className="text shadowed-text">Furniture/Upholstrey</div>
+            <NavLink to="/upholstrey-gallery" className="nav-link text-white text-left">Photos</NavLink>
           </div>
         </div>
       </div>
