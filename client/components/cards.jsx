81f885163e1480028725a68132c70cccade2697d
PaulKillian <psk65lava@gmail.com>
2021-01-07 11:45:18 -0800

    responsiveness on homepage, text, etc.

--- client/components/cards.jsx
+++ client/components/cards.jsx
@@ -4,13 +4,14 @@ import { NavLink } from 'react-router-dom';
 function Cards() {
   return (
     <>
-      <div className="d-flex justify-content-center display-4 mt-5">
-        <div className="row">
-          <div data-aos="fade-up" data-aos-duration="2000" className="mr-5 text-white simple shadowed
-            container carpet-s containTL center-card-text">VLM Carpet
-            <div className="overlayL">
+      <div className="mt-5 container">
+        <div className="row d-flex justify-content-around">
+          <div id="cards" data-aos="fade-up" data-aos-duration="2000" className="col-lg-3 col-md-10 mb-3
+            text-white shadowed carpet-s containTL center-card-text">
+            <h1 className="shadowed text-white">Carpet Cleaning</h1>
+            <div className="overlayR">
               <div className="blur">
-                <p className="lead p-5 text-justify">I provide low moisture carpet cleaning with the use of
+                <p className="lead p-5 text-center">I provide low moisture carpet cleaning with the use of
                   a rotary and or Orbital machine and provide a plant
                   based low moisture encapsulating green cleaner for all
                   types of carpets.<br></br><br></br>The carpets are pre
@@ -23,11 +24,12 @@ function Cards() {
               </div>
             </div>
           </div>
-          <div data-aos="fade-up" data-aos-duration="2000" className="text-white simple shadowed
-            upholstrey-s containTR center-card-text">Furniture/Upholstery
+          <div id="cards" data-aos="fade-up" data-aos-duration="2000" className="col-lg-3 col-md-10 mb-3
+            shadowed upholstrey-s containTR center-card-text text-white">
+            <h1 className="text-white shadowed">Furniture Upholstery</h1>
             <div className="overlayR">
               <div className="blur">
-                <p className="lead p-5 text-justify">I provide low moisture carpet cleaning with the use of
+                <p className="lead p-5 text-center">I provide low moisture carpet cleaning with the use of
                 a rotary and or Orbital machine and provide a plant
                 based low moisture encapsulating green cleaner for all
                 types of carpets. The carpets are pre
@@ -38,7 +40,20 @@ function Cards() {
                 the carpet fibers.</p>
                 <NavLink to="/upholstrey-gallery" className="nav-link text-white text-center">Photos</NavLink>
               </div>
-           </div>
+            </div>
+          </div>
+          <div id="cards" data-aos="fade-up" data-aos-duration="2000" className="col-lg-3 col-md-10 mb-3
+            shadowed grout-s containBR center-card-text text-white">
+            <h1 className="text-white shadowed">Appliance Cleaning</h1>
+            <div className="overlayR">
+              <div className="blur">
+                <p className="lead p-5 text-center">Providing cleaning with a chemical free cleaner
+                and sanitizing with steam for refrigerators and freezers. For ovens and stove tops
+                 I use a non toxic bio degradable cleaner/degreaser  followed up with steam for a
+                 complete sanitization. </p>
+                <NavLink to="/upholstrey-gallery" className="nav-link text-white text-center">Photos</NavLink>
+              </div>
+            </div>
           </div>
         </div>
       </div>
