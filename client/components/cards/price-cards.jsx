ca535c028ce1c4b9a846fc6fdcc09a1072cf3c9b
Paul_Killian <57373172+PaulKillian@users.noreply.github.com>
2021-01-26 12:15:34 -0800

    changes

--- client/components/cards/price-cards.jsx
+++ client/components/cards/price-cards.jsx
@@ -5,10 +5,10 @@ import { faCar } from '@fortawesome/free-solid-svg-icons';
 function PriceCards() {
   return (
     <div className={'container mt-5 mb-5'}>
-      <div className={'row col-lg-12 col-md-6 d-flex justify-content-around'}>
+      <div className={'row col-lg-12 col-md-7 d-flex justify-content-around'}>
         <ul className={'top-ul d-flex flex-wrap'}>
-          <li className={'mr-2 '}><FontAwesomeIcon icon={faCar} className={'fa-3x text-primary'}/>sub/compact 2 door vehicles</li>
-          <li className={'mr-2 '}><FontAwesomeIcon icon={faCar} className={'fa-3x text-success'}/>mid/4 door sedans &#38; hatchbacks</li>
+          <li className={'mr-2'}><FontAwesomeIcon icon={faCar} className={'fa-3x text-primary'}/>sub/compact 2 door vehicles</li>
+          <li className={'mr-2'}><FontAwesomeIcon icon={faCar} className={'fa-3x text-success'}/>mid/4 door sedans &#38; hatchbacks</li>
           <li><FontAwesomeIcon icon={faCar} className={'fa-3x text-warning'}/>full/4 door sedans &#38; small SUV&#39;s</li>
         </ul>
       </div>
@@ -18,8 +18,8 @@ function PriceCards() {
             <h4 className={'p-2 text-center bg-dark price-shadow rounded'}>Car Wash</h4>
           </div>
           <ul className={'py-4 price-shadow pr-4 bg-white'}>
-            <li>Complete Hand wash and dry</li>
-            <li>Clean and Detail wheels &amp; Dress Tires</li>
+            <li>Complete hand wash and dry</li>
+            <li>Clean and detail wheels &amp; dress Tires</li>
             <li>Clean door jambs</li>
             <li>Clean exterior glass and windows</li>
           </ul>
@@ -46,7 +46,7 @@ function PriceCards() {
           </div>
           <ul className={'py-4 pr-4 bg-white price-shadow'}>
             <li>Thorough dusting &amp; vacuuming</li>
-            <li>Vacuum Trunk area (optional)</li>
+            <li>Vacuum trunk area (optional)</li>
             <li>Wiping down all interior surfaces with interior detailer or</li>
             <li>APC - All purpose cleaner or detail cleaning product.
               (Depending on level of soil on surfaces)</li>
@@ -79,15 +79,15 @@ function PriceCards() {
           </div>
           <ul className={'py-4 pr-4 bg-white price-shadow'}>
             <li>Thorough dusting &amp; vacuuming</li>
-            <li>Vacuum Trunk area (optional)</li>
+            <li>Vacuum trunk area (optional)</li>
             <li>Clean and Steam all plastic side panels, door panels and center console</li>
             <li>Dust and clean dashboard, steering wheel &amp; column, all instrument
               panels, glove box, rear view mirror, visors, vents, and headliner</li>
             <li>Clean all fabric surfaces (seats, carpets, mats)</li>
             <li>Clean and condition leather surfaces</li>
-            <li>Clean &amp; Steam Gas and Brake pedals</li>
+            <li>Clean, steam gas and brake pedals</li>
             <li>Apply a UV protective coating to all plastic and vinyl surfaces.
-              (Dash, Door panels, Console, Steering column area, side panels, and all
+              (dash, door panels, console, steering column area, side panels, and all
               other trim.)</li>
           </ul>
         </div>
@@ -113,14 +113,14 @@ function PriceCards() {
           </div>
           <div className={'py-4 pr-4 bg-white price-shadow'}>
             <ul>
-              <li>Step 1. Clay towel, Clay disc or Clay bar to remove embedded
+              <li>Step 1. Clay towel, clay disc or clay bar to remove embedded
               contaminants from the clear coat and windshield</li>
-              <li>Step 2. Clean all Clayed surfaces with a paint prep or APA which is an
+              <li>Step 2. Clean all clayed surfaces with a paint prep or APA which is an
               alcohol wash and distilled to remove any particles left behind on the
               surface prior to polishing</li>
               <li>Step 3. One-step polish (Using a D/A dual action polisher) with an AIO
               all in one glaze polish seal or all in one polish finishing with a sealant
-              wax or Graphene coating for up to 6 months of clear coat protection</li>
+              wax or graphene coating for up to 6 months of clear coat protection</li>
             </ul>
           </div>
         </div>
