5ed16be43f7851705076a40c69dcc9c4a2e48e56
Paul_Killian <57373172+PaulKillian@users.noreply.github.com>
2021-01-26 15:21:19 -0800

    glass

--- client/components/reviews.jsx
+++ client/components/reviews.jsx
@@ -9,33 +9,33 @@ export default function Review() {
         </div>
       </div>
       <div className='col-sm-9 col-md-11 col-lg-8 mt-5 container text-white text-left'>
-        <div className={'mb-5'}>
+        <div className={'mb-5 py-3 blur2 shadowed'}>
           <h5 className={'px-3'}>Ken has been a vendor for our company for several years. He is prompt, trustworthy and does a thorough and comprehensive job. He also is proactive. If he sees a problem he will point it out and help us resolve it or point us to someone who can. We have been very pleased with his service and his ongoing consistency. You can count on Ken to do what he says in a timely and efficient manner.
             <br></br><br></br><span className={'text-center'}><h5>Guy Baker - Wealth Teams Solutions, Irvine CA</h5></span></h5>
         </div>
-        <div className={'mb-5'}>
+        <div className={'mb-5 py-3 blur2 shadowed'}>
           <h5 className={'px-3'}>Ken and his team are amazing with taking care of our carpets at our business.  We have high traffic areas, and Ken always makes sure we are satisfied with each service.  Try Ken and his team out, you won&apos;t be disappointed!
             <br></br><br></br><span className={'text-center'}><h5>Triton Investment Group, San Clemente CA</h5></span></h5>
         </div>
-        <div className={'mb-5'}>
+        <div className={'mb-5 py-3 blur2 shadowed'}>
           <h5 className={'px-3'}>Ken Mendoza has been cleaning our carpets and servicing our tile for over 5 years and does a fantastic job. He is thorough and will spend the extra time needed to get out the tough stains. He is super flexible and has been fantastic at serving our businesses and keeping our carpet and tile in amazing shape!
             <br></br><br></br><span className={'text-center'}><h5>Adam Brody - Academic Chess Academy, Laguna Hills CA</h5></span></h5>
         </div>
-        <div className={'mb-5'}>
+        <div className={'mb-5 py-3 blur2 shadowed'}>
           <h5 className={'px-3'}>I had the pleasure to work with Ken Mendoza for the past two years on many projects including a few residential and multiple commercial jobs. Ken is a very professional individual, always on time, and very knowledgeable about what he does. His carpet cleaning technique is unique and effective. He uses eco/environmental friendly products that doesn’t leave any residue. He provides great services at a very reasonable price. Aside from Carpet cleaning, he helped me with rug cleaning and upholstery cleanings a few times. He always delivered excellent results.
               I highly recommend Ken. I will be happy to be a live or phone reference for his services if necessary.
           <br></br><br></br><span className={'text-center'}><h5>Bita Safari - Simply Crystal Clean, Santa Ana CA</h5></span></h5>
         </div>
-        <div className={'mb-5'}>
+        <div className={'mb-5 py-3 blur2 shadowed'}>
           <h5 className={'px-3'}>We have known Ken Mendoza for about eight years of which we started using Ken&apos;s services about five years.  He has cleaned our carpets at least once a year since, he also has cleaned grout between the floor titles and he has detailed one of our cars. He is a one man operation who puts all his energy into his work and and is extremely detailed in his work. Every project he has done for us we have receive more than we asked for. We highly recommend him.
             <br></br><br></br><span className={'text-center'}><h5>Wayne &amp; Donna Nash,
               Orange CA</h5></span></h5>
         </div >
-        <div className={'mb-5'}>
+        <div className={'mb-5 py-3 blur2 shadowed'}>
           <h5 className={'px-3'}>Ken Mendoza has worked on my car a few times, and I continue to go back to him because his work is top shelf. He is professional, detailed, and his work is amazing, making my 2007 exterior brilliant and like-new with great attention to detail. Next time, I am definitely going to have him do the full detail, because I know I&apos;ll be as pleased with the interior. I highly recommend Ken to anyone who wants their car restored to a striking condition. This guy knows his stuff!
             <br></br><br></br><span className={'text-center'}><h5>Jeff Scattareggia</h5></span></h5>
         </div>
-        <div>
+        <div className={'mb-5 py-3 blur2 shadowed'}>
           <h5 className={'px-3'}>Ken has been washing and detailing my car for a number of months now.  His work has been fabulous and I have received comments on how good my car looks.  I
               have him come to my home once a month and he either washes or details my car.  I depend on him to tell me the best way to keep my car looking good.  He is
               someone I trust to take care of my car.  I would not take my car anywhere else now.  It is so convenient and it costs me less to have him come to my house
