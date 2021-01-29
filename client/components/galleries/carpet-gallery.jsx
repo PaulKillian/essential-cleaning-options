b40b2a8a138c61612c4aedc19f6b04052f47f262
Paul_Killian <57373172+PaulKillian@users.noreply.github.com>
2021-01-29 10:23:18 -0800

    finished

--- client/components/galleries/carpet-gallery.jsx
+++ client/components/galleries/carpet-gallery.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Gallery from 'react-grid-gallery';
+
+useEffect(() => {
+  document.title = 'Essential Cleaning Options: Carpet cleaning photos, Orange County';
+});
 
 function CarpetGallery() {
 
