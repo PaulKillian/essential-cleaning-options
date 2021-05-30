46c453abed5560da52bc555c49255b6a4c87569d
Paul_Killian <psk65lava@gmail.com>
2021-05-29 18:31:58 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -98,7 +98,10 @@ module.exports = {
                 }
               ]
             ],
-            plugins: ['@babel/plugin-transform-react-jsx']
+            plugins: [
+              '@babel/plugin-transform-react-jsx',
+              "@babel/plugin-proposal-object-rest-spread"
+              ]
           }
         }
       }
